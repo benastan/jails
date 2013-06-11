e711f1db914845fe5da62466ec3377f0f5e51419
benastan <bennyjbergstein@gmail.com>
2013-06-10 23:00:26 -0700

    Commit query too

--- public/test.js
+++ public/test.js
@@ -15636,10 +15636,11 @@ var Query = function(model, dataset) {
       this.model = model;
       this.queryParams = {};
       this.fetchAll = false;
-      this.queue = new Queue(function(cb) { cb.apply(_this); });
-      if (! dataset instanceof Dataset) {
-        dataset = new Dataset(model, query);
+      if (dataset instanceof Dataset === false) {
+        dataset = new Dataset(model, this);
       }
+      this.queue = new Queue(function(cb) { cb.apply(_this, this.dataset); });
+      this.dataset = dataset;
     },
     aliasQueryMethods = 'limit page'.split(' ');
 Query.prototype.url = function() {
@@ -15663,12 +15664,14 @@ Query.prototype.query = wrap(function(cb) {
   var model = this.model,
       dataset = this.dataset,
       query = this;
+  this.synced(cb);
   Jails.dataSource({
     type: 'GET',
     url: this.url(),
     data: this.queryParams,
     success: function(res) {
       var currentAttributes;
+      res = JSON.parse(res);
       for (var i in res) {
         currentAttributes = res[i];
         dataset.add(model(currentAttributes));
